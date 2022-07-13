2a8b8cea3ed9d58f4c4e6a37747eb827474c459c
kogonidze <kottok1998@gmail.com>
2022-07-13 14:08:39 +0300

    feat(mapInfoMicroservice): added mapInfoController

--- SPLAT.MapInfo/SPLAT.MapInfo/Program.cs
+++ SPLAT.MapInfo/SPLAT.MapInfo/Program.cs
@@ -1,15 +1,16 @@
+using GoogleMapInfo.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddTransient<GoogleDistanceApi>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-var googleApiKey = builder.Configuration["googleDistanceApi:apiKey"];
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
