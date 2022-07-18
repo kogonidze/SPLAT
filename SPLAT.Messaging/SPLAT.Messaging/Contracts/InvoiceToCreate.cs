80ee94920bfabea9f7da36bb191c7f29e7194ac2
Vasili <kottok1998@gmail.com>
2022-07-18 20:21:06 +0300

    feat(messaging): added contract for InvoiceCreated

---
+++ SPLAT.Messaging/SPLAT.Messaging/Contracts/InvoiceToCreate.cs
@@ -0,0 +1,9 @@
+﻿namespace SPLAT.Messaging.Contracts
+{
+    public interface InvoiceToCreate
+    {
+        int CustomerNumber { get; set; }
+
+        List<InvoiceItems> InvoiceItems { get; set; }
+    }
+}
