9b84803778f4fb8d0399b30046d8a974144f704b
kogonidze <kottok1998@gmail.com>
2022-07-20 17:11:54 +0300

    feat(payment): added fakeBank web api

---
+++ SPLAT.Payment/SPLAT.Payment.FakeBank/Models/Request/ProcessPaymentRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SPLAT.Payment.FakeBank
+{
+    public class ProcessPaymentRequest
+    {
+        public int ClientId { get; set; }
+
+        public double Price { get; set; }
+    }
+}
---
+++ SPLAT.Payment/SPLAT.Payment.FakeBank/ProcessService.cs
@@ -0,0 +1,20 @@
+﻿namespace SPLAT.Payment.FakeBank
+{
+    public class ProcessService
+    {
+        public ProcessService()
+        {
+
+        }
+
+        public void ProcessPayment(int clientId, double price)
+        {
+            using (FakeBankContext db = new FakeBankContext())
+            {
+                var account = db.Accounts.Find(clientId);
+
+                //if (account == null)  
+            }
+        }
+    }
+}
