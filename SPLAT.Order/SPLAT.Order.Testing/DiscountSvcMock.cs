b95eb520470a07b855d6df1c013f963d95cf8754
kogonidze <kottok1998@gmail.com>
2022-08-11 17:44:26 +0300

    feat(order): added project for consumer-driven contract testing

---
+++ SPLAT.Order/SPLAT.Order.Testing/DiscountSvcMock.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using PactNet;
+using PactNet.Mocks.MockHttpService;
+
+namespace SPLAT.Order.Testing
+{
+    public class DiscountSvcMock : IDisposable
+    {
+        private readonly IPactBuilder _pactBuilder;
+        private readonly int _servicePort = 9222;
+        private bool _disposed = false;
+
+        public IMockProviderService MockProviderService { get; }
+
+        public string ServiceUri => $"http://localhost:{_servicePort}";
+
+        public DiscountSvcMock()
+        {
+            var pactConfig = new PactConfig
+            {
+                SpecificationVersion = "2.0.0",
+                PactDir = @"c:\temp\pact\OrderSvcConsumer",
+                LogDir = @"c:\temp\pact\OrderSvcConsumer\logs"
+            };
+
+            _pactBuilder = new PactBuilder(pactConfig)
+                .ServiceConsumer("Orders")
+                .HasPactWith("Discounts");
+
+            MockProviderService = _pactBuilder.MockService(_servicePort,
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _pactBuilder.Build();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
